2d06c34913fbee4a56e8d14363e61fa22da5bc21
Aadi0215 <72636522+Aadi0215@users.noreply.github.com>
2022-07-04 11:35:27 -0400

    Update class125two.py

--- class125two.py
+++ class125two.py
@@ -8,7 +8,7 @@ def predict_data():
     prediction = get_prediction(image)
     return jsonify({
         'prediction':prediction
-    },200)
+    }), 200
 
 if __name__=='__main__':
     app.run(debug=True)
